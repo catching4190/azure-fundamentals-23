8d3130751e6978b8d1c88614a6d74cce75c54de4
Viacheslav Glazhevskyi <viacheslav.glazhevskyi@altran.com>
2023-05-09 21:55:10 +0300

    Update postdeploy script

--- 04/postdeploy.sh
+++ 04/postdeploy.sh
@@ -3,7 +3,8 @@ echo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo apt-get install unzip
-wget -O /tmp/app.zip https://github.com/catching4190/azure-fundamentals-23/raw/5b918c636b5df6094ca6f49bc7514f5f36a799c7/04/app.zip
-sudo unzip /tmp/app.zip -o -d /
+wget -O ~/app.zip https://github.com/catching4190/azure-fundamentals-23/raw/d0b30d87b46ad69beabad5901c2c4286496f9899/04/app.zip
+wget -O /etc/nginx/nginx.conf https://github.com/catching4190/azure-fundamentals-23/raw/d0b30d87b46ad69beabad5901c2c4286496f9899/04/nginx.conf
+unzip app.zip
 sudo systemctl enable nginx
 sudo systemctl start nginx
